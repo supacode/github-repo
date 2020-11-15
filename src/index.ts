28173c09602c4bbba8bf46c9c80982e22dfa52de
Maverick <supacode@gmail.com>
2020-11-15 17:28:37 +0100

    Add readme

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const API_URL = 'https://api.github.com/graphql';
-const OAUTH_TOKEN = '9a43860637e87360b9256ed06b89f285b164b842';
+const OAUTH_TOKEN = '2bef411ec162d298cf3d86b0d931c9009c7a2af5';
 
 import { getData, elements } from './globals';
 import { UI } from './classes';
